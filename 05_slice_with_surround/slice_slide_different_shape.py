bf815b31a946b800475ee3132aca10d1f9ea79a8
Spritea <461158649@qq.com>
2019-04-02 22:19:55 +0800

    slice with different shape

--- 05_slice_with_surround/slice_slide_different_shape.py
+++ 05_slice_with_surround/slice_slide_different_shape.py
@@ -4,18 +4,18 @@ from pathlib import Path
 import natsort
 from tqdm import tqdm
 
+#把多的边边单独割出来
 def slice_slide_one(img_path):
     out_height = 512
     out_width = 512
     #for validate set, overlap=0
-    overlap_height=100
-    overlap_width=100
+    overlap_height=0
+    overlap_width=0
     slide_height=out_height-overlap_height
     slide_width=out_width-overlap_width
     # slide_height=100
     # slide_width=100
     k = 0
-    end=0
     im = Image.open(img_path)
     imgwidth, imgheight = im.size
     for i in range(0, imgheight, slide_height):
@@ -23,20 +23,18 @@ def slice_slide_one(img_path):
             if j + out_width <= imgwidth and i + out_height <= imgheight:
                 box = (j, i, j + out_width, i + out_height)
             elif j + out_width > imgwidth and i + out_height <= imgheight:
-                box = (imgwidth - out_width, i, imgwidth, i + out_height)
-                end=1
+                box = (j, i, imgwidth, i + out_height)
             elif j + out_width <= imgwidth and i + out_height > imgheight:
-                box = (j, imgheight - out_height, j + out_width, imgheight)
+                box = (j, i, j + out_width, imgheight)
             else:
-                box = (imgwidth - out_width, imgheight - out_height, imgwidth, imgheight)
-                end=2
+                box = (j, i, imgwidth, imgheight)
             a = im.crop(box)
             first_name=str(Path(img_path).stem).zfill(4)
-            a.save(os.path.join("Vahingen/test_out/", first_name+"_%04d.png" % k))
+            a.save(os.path.join("Postdam/train_18_val_20/different_shape_val/", first_name+"_%04d.png" % k))
             k += 1
 
 
-IMG_Path = Path("Vahingen/test/")
+IMG_Path = Path("E:\code\hotkey\\17_post_proc\Postdam\\from-pytorch-train\data14\\val")
 IMG_File = natsort.natsorted(list(IMG_Path.glob('*.tif')))
 IMG_Str = []
 for i in IMG_File:
--- 16_find_compact/detect_color.py
+++ 16_find_compact/detect_color.py
@@ -1,5 +1,5 @@
 from PIL import Image
 
-image = Image.open("C:\\Users\dell\Desktop\\folder\last\\10.bmp")
+image = Image.open("E:\code\hotkey\\18_preprocess\Postdam\\train_24_val_14\scale075\\rotate\precode_train_label\scale075_r270-02899.png")
 a=image.getcolors()
 print(image.getcolors())
